4fe4d1de11e99bd84ac900ba5724cc8852505aca
DupciaBlada <wgarbowicz@edu.cdv.pl>
2023-12-20 08:26:07 +0100

    \zdxas
    asd

--- Assets/KolizjaPostaci.cs
+++ Assets/KolizjaPostaci.cs
@@ -7,7 +7,7 @@ public class KolizjaPostaci : MonoBehaviour
         if (other.CompareTag("NazwaTaguObiektu"))
         {
             // Tutaj mo¿esz umieœciæ kod reakcji na kolizjê z danym obiektem
-            Debug.Log("Kolizja z obiektem o tagu: " + other.tag);
+            //Debug.Log("Kolizja z obiektem o tagu: " + other.tag);
 
             // Przyk³ad: Zniszcz obiekt po kolizji
             Destroy(other.gameObject);
--- Assets/RedFollowCharacter.cs
+++ Assets/RedFollowCharacter.cs
@@ -37,4 +37,19 @@ public class RedFollowCharacter : MonoBehaviour
             }
         }
     }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isCollidingWithPlayer = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isCollidingWithPlayer = false;
+        }
+    }
 }
