825e079c562a8ecffc484221cfa0db110e6a684a
lonephoenix1 <kgolec@edu.cdv.pl>
2024-01-10 11:29:26 +0100

    KamilWolf

--- Assets/scripts/EnemySpawner1.cs
+++ Assets/scripts/EnemySpawner1.cs
@@ -1,46 +1,94 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemySpawner1 : MonoBehaviour
+public class TestKamilWizardMovement : MonoBehaviour
 {
-    [SerializeField]
-    private GameObject _enemyPrefab;
-
-    [SerializeField]
-    private float _minimumSpawnTime;
-
-    [SerializeField]
-    private float _maximumSpawnTime;
-
-    private float _timeUntilSpawn;
-
-    public int totalEnemyCount = 5; 
-    private int generatedEnemyCount = 0;
+    public Transform targetCharacter; // Referencja do postaci, za któr¹ pod¹¿amy
+    public float moveSpeed = 3.0f;
+    public float detectionRadius = 5.0f; // Promieñ detekcji postaci
+    public float stopFollowRadius = 7.0f; // Dodatkowy promieñ, po przekroczeniu którego przeciwnik przestaje œledziæ postaæ
+    public float minDistance = 2.0f; // Minimalny dystans, na jaki przeciwnik utrzymuje od gracza
+    private SpriteRenderer rbSprite;
+    private Animator animator;
+    private Rigidbody2D rb;
+    private bool isFollowing = false; // Flaga okreœlaj¹ca, czy przeciwnik powinien pod¹¿aæ
 
     void Start()
     {
-        SetTimeUntilSpawn();
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        rbSprite = GetComponent<SpriteRenderer>();
     }
 
     void Update()
     {
-        if (generatedEnemyCount < totalEnemyCount)
+        if (targetCharacter != null)
         {
-            _timeUntilSpawn -= Time.deltaTime;
+            float distanceToTarget = Vector3.Distance(transform.position, targetCharacter.position);
 
-            if (_timeUntilSpawn <= 0)
+            // Sprawdzamy, czy postaæ jest w zasiêgu
+            if (distanceToTarget <= detectionRadius)
             {
-                Instantiate(_enemyPrefab, transform.position, Quaternion.identity);
-                SetTimeUntilSpawn();
-                generatedEnemyCount++;
+                isFollowing = true; // W³¹czamy tryb œledzenia
             }
+
+            // Jeœli przeciwnik jest w trybie œledzenia
+            if (isFollowing)
+            {
+                // Obliczamy kierunek ruchu
+                Vector3 targetPosition = targetCharacter.position;
+                Vector3 moveDirection = (targetPosition - transform.position).normalized;
+
+                // Obliczamy dystans do postaci
+                float distanceToPlayer = Vector3.Distance(transform.position, targetCharacter.position);
+
+                // Jeœli dystans do postaci jest wiêkszy ni¿ minimalny dystans
+                if (distanceToPlayer > minDistance)
+                {
+                    // Zmniejszamy dystans do postaci do minimalnego dystansu
+                    transform.position += moveDirection * (distanceToPlayer - minDistance) * moveSpeed * Time.deltaTime;
+                }
+            }
+        }
+
+        Vector2 enemyMovement = GetEnemyMovementVector();
+
+        animator.SetFloat("Horizontal", enemyMovement.x);
+        animator.SetFloat("Vertical", enemyMovement.y);
+        animator.SetFloat("speed", enemyMovement.sqrMagnitude);
+
+        if (enemyMovement != Vector2.zero)
+        {
+            // Ustawiamy flipX w zale¿noœci od kierunku ruchu
+            rbSprite.flipX = enemyMovement.x < 0;
         }
     }
 
-    private void SetTimeUntilSpawn()
+    Vector2 GetEnemyMovementVector()
     {
-        _timeUntilSpawn = Random.Range(_minimumSpawnTime, _maximumSpawnTime);
+        if (targetCharacter != null)
+        {
+            float distanceToTarget = Vector3.Distance(transform.position, targetCharacter.position);
+
+            // Sprawdzamy, czy postaæ jest w zasiêgu
+            if (distanceToTarget <= detectionRadius)
+            {
+                // Obliczamy kierunek ruchu
+                Vector3 targetPosition = targetCharacter.position;
+                Vector3 moveDirection = (targetPosition - transform.position).normalized;
+
+                // Zwracamy wektor ruchu przeciwnika
+                return new Vector2(moveDirection.x, moveDirection.y);
+            }
+            else if (isFollowing && distanceToTarget <= stopFollowRadius)
+            {
+                // Jeœli przeciwnik przekroczy³ pierwotny zasiêg, ale jest w nowym zasiêgu, nadal œledŸ postaæ
+                Vector3 targetPosition = targetCharacter.position;
+                Vector3 moveDirection = (targetPosition - transform.position).normalized;
+                return new Vector2(moveDirection.x, moveDirection.y);
+            }
+        }
+
+        // Jeœli przeciwnik nie porusza siê, zwracamy wektor zerowy
+        return Vector2.zero;
     }
 }
-
---
+++ Assets/scripts/EnemySpawner2.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner2 : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _enemyPrefab;
+
+    [SerializeField]
+    private float _minimumSpawnTime;
+
+    [SerializeField]
+    private float _maximumSpawnTime;
+
+    private float _timeUntilSpawn;
+
+    public int totalEnemyCount = 5; // Okreœlona iloœæ przeciwników do wygenerowania
+    private int generatedEnemyCount = 0;
+
+    public Transform playerTransform; // Referencja do transformacji gracza
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetTimeUntilSpawn();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (generatedEnemyCount < totalEnemyCount)
+        {
+            _timeUntilSpawn -= Time.deltaTime;
+
+            if (_timeUntilSpawn <= 0)
+            {
+                // Tworzymy przeciwnika
+                GameObject enemy = Instantiate(_enemyPrefab, transform.position, Quaternion.identity);
+
+                // Przypisujemy transformacjê gracza do przeciwnika
+                enemy.GetComponent<EnemyWizardMovement>().targetCharacter = playerTransform;
+
+
+
+                SetTimeUntilSpawn();
+                generatedEnemyCount++;
+                Debug.Log(generatedEnemyCount);
+            }
+        }
+    }
+
+    private void SetTimeUntilSpawn()
+    {
+        _timeUntilSpawn = Random.Range(_minimumSpawnTime, _maximumSpawnTime);
+    }
+}
