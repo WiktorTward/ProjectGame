b6fbae0631509edcd900a5eee97d5f97a837e86a
lonephoenix1 <kgolec@edu.cdv.pl>
2024-01-17 10:14:25 +0100

    Staradded
    trzeba usunąć // w skrypcie dla gwiazdki by działało

---
+++ Assets/scripts/StarCollectible.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarCollectible : MonoBehaviour
+{
+    public int scoreValue = 100;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            //ScoreManager.Instance.AddScore(scoreValue);
+
+            Destroy(gameObject);
+        }
+    }
+}
+
